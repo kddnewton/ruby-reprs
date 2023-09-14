4e41374541e86ab3f65867c15d1b9e9ee659a7e8
Kevin Newton <kddnewton@gmail.com>
2023-09-13 21:17:50 -0400

    Use YARP inspect

--- server.rb
+++ server.rb
@@ -8,6 +8,10 @@ require "ruby_parser"
 require "syntax_tree"
 require "yarp"
 
+def pp_object(object)
+  PP.pp(object, +"", 79)
+end
+
 def parse(code, types)
   types.to_h do |type|
     parsed =
@@ -20,21 +24,22 @@ def parse(code, types)
         lexer = Parser::Lexer.new(32)
         lexer.source_buffer = buffer
 
-        [].tap do |tokens|
-          token = nil
-          tokens << token until ((token = lexer.advance) in [false, *])
-        end
-      in "RL"  then Ripper.lex(code)
-      in "RSR" then Ripper.sexp_raw(code)
-      in "RS"  then Ripper.sexp(code)
-      in "RP"  then RubyParser.new.parse(code)
-      in "RVA" then RubyVM::AbstractSyntaxTree.parse(code)
-      in "ST"  then SyntaxTree.parse(code)
-      in "Y"   then YARP.parse(code).value
-      in "YL"  then YARP.lex(code).value
+        tokens = []
+        token = nil
+
+        tokens << token until ((token = lexer.advance) in [false, *])
+        pp_object(tokens)
+      in "RL"  then pp_object(Ripper.lex(code))
+      in "RSR" then pp_object(Ripper.sexp_raw(code))
+      in "RS"  then pp_object(Ripper.sexp(code))
+      in "RP"  then pp_object(RubyParser.new.parse(code))
+      in "RVA" then pp_object(RubyVM::AbstractSyntaxTree.parse(code))
+      in "ST"  then pp_object(SyntaxTree.parse(code))
+      in "Y"   then YARP.parse(code).value.inspect
+      in "YL"  then pp_object(YARP.lex(code).value)
       end
 
-    [type, PP.pp(parsed, +"", 79)]
+    [type, parsed]
   rescue
     [type, $!.message]
   end
